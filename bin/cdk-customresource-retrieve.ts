d64b96a2379dd66a1f3649b5e5a571afa3782646
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-01-29 23:14:59 +0000

    upload source

--- bin/cdk-customresource-retrieve.ts
+++ bin/cdk-customresource-retrieve.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { CdkCustomresourceRetrieveStack } from '../lib/cdk-customresource-retrieve-stack';
 
 const app = new cdk.App();
-new CdkCustomresourceRetrieveStack(app, 'CdkCustomresourceRetrieveStack');
+new CdkCustomresourceRetrieveStack(app, 'CdkCustomresourceRetrieveStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "us-east-1",
+    },
+});
--- lib/cdk-customresource-retrieve-stack.ts
+++ lib/cdk-customresource-retrieve-stack.ts
@@ -1,9 +1,48 @@
 import * as cdk from '@aws-cdk/core';
 
+import { Code, Function as LambdaFunction, Runtime } from "@aws-cdk/aws-lambda";
+import { CfnOutput, CustomResource, Duration } from '@aws-cdk/core';
+import { Provider } from '@aws-cdk/custom-resources';
+
 export class CdkCustomresourceRetrieveStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const lambdaCode = `
+var response = require('cfn-response');
+exports.onEvent = function(event, context) {
+  var responseData = {"myatt": "test"};
+  response.send(event, context, response.SUCCESS, responseData);
+};
+    `;
+
+    const onEventFunction = new LambdaFunction(this, "OnEventLambda", {
+      code: Code.fromInline(lambdaCode),
+      handler: "index.onEvent",
+      runtime: Runtime.NODEJS_12_X,
+    });
+
+    const provider = new Provider(this, "Provider", {
+      onEventHandler: onEventFunction,
+    });
+
+    const customResource = new CustomResource(this, "CustomResource", {
+      serviceToken: provider.serviceToken,
+      resourceType: "Custom::Test",   // !!!!
+    });
+    
+    const customResource2 = new CustomResource(this, "CustomResource2", {
+      serviceToken: provider.serviceToken,
+      //resourceType: "Custom::Test",   // worked
+    });
+    
+    
+    const output = customResource2.getAttString("myatt"); 
+    
+    const c = new CfnOutput(this, "OutputTest", {
+      value: output,
+    });
+    
   }
 }
